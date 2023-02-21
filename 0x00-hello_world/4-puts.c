7b12bb19f5e3d9a651476299c9333997e4973860
DIALLO Amirou Yannick Wendpuire <amiroudiallo.yw@gmail.com>
2023-02-21 15:25:13 -0800

    4. puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 /**
  * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
  * Return 0
  */
-int main(void) {
+
+int main(void)
+{
 	puts ("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
