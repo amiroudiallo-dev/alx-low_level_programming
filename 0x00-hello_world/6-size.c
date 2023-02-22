3d8476374d5a59806560eeb721fd12ad56729ed3
DIALLO Amirou Yannick Wendpuire <amiroudiallo.yw@gmail.com>
2023-02-22 12:12:18 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an integer: %zu byte(s)\n", sizeof(int));
 	printf("Size of long int: %zu byte(s)\n", sizeof(long int));
-	print("Size of long long: %zu byte(s)\n", sizeof(long long));
+	printf("Size of long long: %zu byte(s)\n", sizeof(long long));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
