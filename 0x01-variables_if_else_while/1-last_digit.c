237716838a3b16882628befae5339b2d15663d34
DIALLO Amirou Yannick Wendpuire <amiroudiallo.yw@gmail.com>
2023-02-25 12:21:05 -0800

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: last digit
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	result = n % 10;
+
+	if (result > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, result);
+	} else if (result == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, result);
+	} else
+	{
+		printf("Last digitof %d is %d and is less than 6 and not 0\n", n, result);
+	}
+	return (0);
+}
