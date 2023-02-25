8b91a753b1c3536e88e6eff23e6b7b3a820f6040
DIALLO Amirou Yannick Wendpuire <amiroudiallo.yw@gmail.com>
2023-02-25 14:38:39 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: program that prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 10; i < 20; i++ )
+	{
+		putchar((i % 10) + '0');
+		if (i != 19 )
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
