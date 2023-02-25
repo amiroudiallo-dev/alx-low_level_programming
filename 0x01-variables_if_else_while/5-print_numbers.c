ba1a99dd499bf826b187bb05cce32782603c630e
DIALLO Amirou Yannick Wendpuire <amiroudiallo.yw@gmail.com>
2023-02-25 13:38:42 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit numbers
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	for (int a = 0; a < 10; a++)
+		printf("%d" ,a);
+	putchar('\n');
+	return (0);
+}
