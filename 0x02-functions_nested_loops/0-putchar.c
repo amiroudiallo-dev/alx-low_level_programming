abd3f54297fb032dea54ace9cc37d3a8c3bc9930
DIALLO Amirou Yannick Wendpuire <amiroudiallo.yw@gmail.com>
2023-02-26 09:27:22 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+* putchar - write the character c to stdout
+*
+* @c: The character to print
+*
+* Return: 0 (success)
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
