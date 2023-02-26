123116a1b5bd8bb30dc354ae6736483da8c900a2
DIALLO Amirou Yannick Wendpuire <amiroudiallo.yw@gmail.com>
2023-02-26 09:39:38 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <main.h>
 
 /**
 * putchar - write the character c to stdout
@@ -8,13 +8,16 @@
 * Return: 0 (success)
 */
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 int main(void)
 {
-	char c;
-	_putchar(c);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n')
+	return (0);
 }
