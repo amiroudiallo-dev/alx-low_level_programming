090ad1537ff7f9c9fffe0c06c83fd69854bd5205
DIALLO Amirou Yannick Wendpuire <amiroudiallo.yw@gmail.com>
2023-02-26 15:38:20 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase character
+ *
+ * @c : parameter to check
+ *
+ * Return: 1 if islower 0 isupper
+ */
+
+int _islower(int c)
+{
+	if (c => 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
