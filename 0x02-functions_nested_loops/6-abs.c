23b6068945a9d9fb5ab6793483df1a23cbab1624
DIALLO Amirou Yannick Wendpuire <amiroudiallo.yw@gmail.com>
2023-02-26 16:06:25 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute of an integer
+ *
+ * @int: parameter
+ *
+ * Return: always 0 (success)
+ */
+
+int _abs(int n)
+{
+	if ( n >= 0)
+	{
+		retrun (n);
+	}
+	else
+	{
+		return (-n);
+	}
+	
+	_putchar('\n');
+}	
