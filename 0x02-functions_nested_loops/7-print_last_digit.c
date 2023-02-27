b0ebaed7e440e4ca05c668a1d3f749f8c6419762
DIALLO Amirou Yannick Wendpuire <amiroudiallo.yw@gmail.com>
2023-02-26 16:26:39 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "_abs.c"
 
 /**
  * print_last_digit - method print the last digit of a number
@@ -10,12 +11,6 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-	{
-		n *= -1;
-	}
-
-	_putchar('0' + (n % 10));
-
-	return (n % 10);
+	_putchar('0' + _abs(n % 10));
+	return (_abs(n % 10));
 }	
