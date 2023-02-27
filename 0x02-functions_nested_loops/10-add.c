4e1d948556f19708278a47cc18637640020e4897
DIALLO Amirou Yannick Wendpuire <amiroudiallo.yw@gmail.com>
2023-02-26 17:12:47 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function that add two number
+ *
+ * @a : parameter number one
+ *
+ * @b : parameter number two
+ *
+ * Return: Integer as the result of sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
