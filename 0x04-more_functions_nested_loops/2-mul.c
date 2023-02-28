6e09a27f1e2e276725583d07e3d296e37c331a40
DIALLO Amirou Yannick Wendpuire <amiroudiallo.yw@gmail.com>
2023-02-28 13:29:17 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integer
+ *
+ * @a: first parameter
+ * @b: second parameter
+ * Return : the result of x and y
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
