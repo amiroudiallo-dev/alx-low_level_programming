32b2191b54d47c3dc92b18b9988c6bd19db964af
amiroudiallo-dev <amiroudiallo.yw@gmail.com>
2023-09-08 16:23:21 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space.
+ * main - Prints all possible combinations of two different digits, in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
@@ -19,13 +18,10 @@ int main(void)
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;
-			
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
