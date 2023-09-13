c7ad10051246579992e9e184092ad52536dda68c
amiroudiallo-dev <amiroudiallo.yw@gmail.com>
2023-09-13 09:57:42 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,21 @@
 #include "main.h"
-#include <unistd.h>
 
-int _putchar(char c)
+/**
+ * main - print School, followed by new line
+ * Return: 0
+ */
+
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
