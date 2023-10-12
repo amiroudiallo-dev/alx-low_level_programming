b3698057c945553210f7684fe50dd9eb32e7f08f
amiroudiallo-dev <amiroudiallo.yw@gmail.com>
2023-10-12 15:07:34 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -89,13 +89,3 @@ void print_all(const char * const format, ...)
 	printf("\n");
 	va_end(anyArgs);
 }
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_all("ceis", 'B', 3, "stSchool");
-    return (0);
-}
