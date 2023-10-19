7e31ce8b3b284fc4e25138f6207c95767db5ecd5
amiroudiallo-dev <amiroudiallo.yw@gmail.com>
2023-10-19 07:47:55 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,6 @@
 section .data
     hello db 'Hello, Holberton', 0
+    newline db 10               ; ASCII code for newline
 
 section .text
     global main
@@ -9,6 +10,9 @@ main:
     sub rsp, 8                  ; Align the stack
     mov rdi, hello              ; Format string
     call printf                 ; Call printf
+    mov rdi, newline            ; Format string for newline
+    call printf                 ; Call printf for newline
     add rsp, 8                  ; Restore the stack
     mov eax, 0                  ; Return 0
     ret
+
