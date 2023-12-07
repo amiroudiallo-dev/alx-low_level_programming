1110b55b74cb0875a5943adfcef8f26c50b0f93a
amiroudiallo-dev <amiroudiallo.yw@gmail.com>
2023-12-07 02:26:41 -0800

    dd

---
+++ 0x17-doubly_linked_lists/pal.py
@@ -0,0 +1,12 @@
+# Find the largest palindrome made from the product of two 3-digit numbers
+max_palindrome = 0
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        product = i * j
+        product_str = str(product)
+        if product_str == product_str[::-1] and product > max_palindrome:
+            max_palindrome = product
+
+# Save the result in the file 102-result
+with open('102-result', 'w') as file:
+    file.write(str(max_palindrome))
